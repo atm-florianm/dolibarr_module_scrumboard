c8bc7051f0a525e9304b2f5fc0b4aa12f412db23
Gauthier <gauthier.verdol@atm-consulting.fr>
2015-10-14 17:32:10 +0200

    Yiiiihhaaaa !!!!

--- script/scrum.js.php
+++ script/scrum.js.php
@@ -329,7 +329,7 @@ function pop_time(id_project, id_task) {
 								
 								jEnd = data.indexOf('"error"', jStart) - 10; 
 								message = data.substr(jStart,  jEnd - jStart).replace(/\\'/g,'\'');
-								$.jnotify(message, "error");
+								$.jnotify('<?php echo $langs->trans('TimeAdded') ?>');
 							}
 							else {
 								$.jnotify('<?php echo $langs->trans('TimeAdded') ?>', "ok");
