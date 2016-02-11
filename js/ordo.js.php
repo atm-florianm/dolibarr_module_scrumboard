d4adc3d629c45572667322994e9662527375cd7e
Alexis Algoud <alexis@atm-consulting.fr>
2016-02-11 15:12:19 +0100

    avoid error

--- js/ordo.js.php
+++ js/ordo.js.php
@@ -365,7 +365,7 @@ function TOrdonnancement() {
 				}
 				
 				current_position = $li.position();
-				if( current_position.top!=task_top || current_position.left!=width_column * task.grid_col || $li.height()!=height ) {
+				if(current_position && (current_position.top!=task_top || current_position.left!=width_column * task.grid_col || $li.height()!=height) ) {
 					//console.log('animate',i, current_position, task_top, width_column * task.grid_col, $li.height(),height);
 					$li.animate({
                         	top:task_top
