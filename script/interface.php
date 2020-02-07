9c554652aae4894f27d36bfc8c4fdf0b46d2b955
Florian Mortgat <florian.mortgat@atm-consulting.fr>
2020-02-07 09:17:34 +0100

    T1978 - moved the code that builds the SQL query for getting tasks to a lib function to make it reusable by the CSV building code

--- script/interface.php
+++ script/interface.php
@@ -398,148 +398,21 @@ function _reset_date_task(&$db, $id_project, $velocity) {
 function _tasks(&$db, $id_project, $status, $fk_user, $fk_soc, $soc_type, $TDateFilters, $search_array_options, $object, $extrafieldstask, $label_filter, $country_filter, $state_filter)
 {
 	global $conf, $hookmanager;
-
-	dol_include_once('scrumboard/class/scrumboard.class.php');
-
-	$sql = 'SELECT DISTINCT pt.rowid, pt.story_k, pt.scrum_status, pt.rang
-			FROM '.MAIN_DB_PREFIX.'projet_task pt
-			INNER JOIN '.MAIN_DB_PREFIX.'projet p ON (p.rowid = pt.fk_projet)';
-
-	if (!empty($search_array_options)) $sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'projet_task_extrafields ef ON (ef.fk_object = pt.rowid)';
-
-	if(empty($id_project) && $status != 'unknownColumn')
-	{
-		$sql.= ' INNER JOIN ' . MAIN_DB_PREFIX . 'projet_storie ps ON (ps.fk_projet = pt.fk_projet AND ps.storie_order = pt.story_k)';
-	}
-
-	if (!empty($conf->global->SCRUM_FILTER_BY_USER_ENABLE) && $fk_user > 0)
-	{
-		$sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'element_contact ec ON (ec.element_id = pt.rowid)';
-		$sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'c_type_contact tc ON (tc.rowid = ec.fk_c_type_contact)';
-	}
-    if ((!empty($country_filter) || !empty($state_filter)) && !empty($search_array_options))
-    {
-        $sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'societe soc ON (ef.fk_etablissement = soc.rowid)';
-    }
-
-	if($status == 'unknownColumn') {
-		$scrumboardColumn = new ScrumboardColumn;
-		$PDOdb=new TPDOdb;
-		$scrumboardColumn->LoadAllBy($PDOdb);
-		$defaultColumn = $scrumboardColumn->getDefaultColumn();
-
-		$sql .= ' WHERE (scrum_status NOT IN (SELECT code FROM '.MAIN_DB_PREFIX.'c_scrum_columns WHERE active=1))';
-	}
-	else {
-		$sql.= ' WHERE 1 ';
-		$sql.= ' AND ((scrum_status IS NOT NULL AND scrum_status = "'.$status.'")';
-
-		if($status=='ideas') $sql.= ' OR (scrum_status IS NULL AND (progress = 0 OR progress IS NULL) AND datee IS NULL)';
-		else if($status=='todo') $sql.= ' OR (scrum_status IS NULL AND  (progress = 0  OR progress IS NULL))';
-		else if($status=='inprogress') $sql.= ' OR (scrum_status IS NULL AND  progress > 0 AND progress < 100)';
-		else if($status=='finish') $sql.= ' OR (scrum_status IS NULL AND  progress=100)';
-		$sql .= ')';
-	}
-
-	if($id_project > 0) $sql.= ' AND fk_projet='.$id_project;
-
-	if (!empty($conf->global->SCRUM_FILTER_BY_USER_ENABLE) && $fk_user > 0)
-	{
-		$sql.= ' AND tc.element = \'project_task\' AND ec.fk_socpeople = '.$fk_user;
-	}
-
-	$parameters = array('id_project' => $id_project, 'fk_soc' => $fk_soc, 'soc_type' => $soc_type);
-	$reshook = $hookmanager->executeHooks('scrumManageFk_socSQL', $parameters, $object, $action);
-	if ($reshook > 0) $sql.=$hookmanager->resPrint;
-	if (empty($reshook) && $fk_soc > 0)
-	{
-		if ($soc_type === 'onlycompany' || $soc_type === 'both')
-		{
-			$sql.= ' AND ';
-			if ($soc_type === 'both') $sql.= ' ( ';
-			$sql.= 'p.fk_soc = '.$fk_soc;
-		}
-
-		if ($soc_type === 'onlychildren' || $soc_type === 'both')
-		{
-			$resql = $db->query('SELECT rowid FROM '.MAIN_DB_PREFIX.'societe WHERE parent = '.$fk_soc);
-			if ($resql)
-			{
-				$TSocId = array();
-				while ($obj = $db->fetch_object($resql))
-				{
-					$TSocId[] = $obj->rowid;
-				}
-
-				if (!empty($TSocId))
-				{
-					if ($soc_type === 'both') $sql.= ' OR ';
-					else $sql.= ' AND ';
-					$sql.= 'p.fk_soc IN ('.implode(',', $TSocId).')';
-				}
-				else
-				{
-					$sql.= 'p.fk_soc = -1';
-				}
-			}
-			else
-			{
-				dol_print_error($db);
-			}
-
-			if ($soc_type === 'both') $sql.= ' ) ';
-		}
-	}
-	// date filter
-	LIST ($start_date_after, $start_date_before, $end_date_after, $end_date_before) = $TDateFilters;
-
-	// add error if date range boundaries are not in the right order (negative range)
-	$startDateNegativeDateRange = !empty($start_date_before) && $start_date_after > $start_date_before;
-	$endDateNegativeDateRange   = !empty($end_date_before)   && $end_date_after   > $end_date_before;
-	if ($startDateNegativeDateRange || $endDateNegativeDateRange)
-	{
-		global $langs;
-		return array(
-			'error' => true,
-			'message' => $langs->trans('FilterErrorNegativeDateRange')
-		);
-	}
-	if (!empty($start_date_after))  $sql .= ' AND pt.dateo >= ' . "'" . $db->idate($start_date_after)  . "'";
-	if (!empty($start_date_before)) $sql .= ' AND pt.dateo <= ' . "'" . $db->idate($start_date_before) . "'";
-	if (!empty($end_date_after))    $sql .= ' AND pt.datee >= ' . "'" . $db->idate($end_date_after)    . "'";
-	if (!empty($end_date_before))   $sql .= ' AND pt.datee <= ' . "'" . $db->idate($end_date_before)   . "'";
-
-	// extrafields filters
-	if (!empty($search_array_options))
-	{
-        $extrafields = &$extrafieldstask; // Compatibility for tpl
-        $action = 'setSqlExtrafields';
-        $parameters = array('sql' => &$sql, 'id_project' => $id_project, 'status' => $status, 'fk_user' => $fk_user, 'fk_soc' => $fk_soc, 'soc_type' => $soc_type, 'TDateFilters' => $TDateFilters, 'search_array_options' => $search_array_options, 'extrafieldstask' => $extrafieldstask, 'label_filter' => $label_filter, 'country_filter' => $country_filter, 'state_filter' => $state_filter);
-        $reshook = $hookmanager->executeHooks('doTasks', $parameters, $object, $action); // Note that $action and $object may have been modified by some
-        if ($reshook < 0) setEventMessages($hookmanager->error, $hookmanager->errors, 'errors');
-
-        if (empty($reshook))
-        {
-            // Add where from extra fields
-            include DOL_DOCUMENT_ROOT.'/core/tpl/extrafields_list_search_sql.tpl.php';
-        }
-	}
-	// filter on label
-	if (!empty($label_filter))
-	{
-		$sql .= ' AND pt.label LIKE \'%' . $db->escape($label_filter) . '%\'';
-	}
-	// filter on state / country
-    if (!empty($country_filter))
-    {
-        $sql .= ' AND soc.fk_pays = ' . $country_filter;
-    }
-    if (!empty($state_filter))
-    {
-        $sql .= ' AND soc.fk_departement = ' . $state_filter;
-    }
-
-	$sql.= ' ORDER BY pt.rang';
+	$sql = getSQLForTasks(
+		$db,
+		$id_project,
+		$status,
+		$fk_user,
+		$fk_soc,
+		$soc_type,
+		$TDateFilters,
+		$search_array_options,
+		$object,
+		$extrafieldstask,
+		$label_filter,
+		$country_filter,
+		$state_filter
+	);
 
 	$res = $db->query($sql);
 
