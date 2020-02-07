58f647e8c100e5a965e36f9972a0ea203914fc6a
Florian Mortgat <florian.mortgat@atm-consulting.fr>
2020-02-07 10:29:03 +0100

    T1978 - move & rename the _task function to the lib to make it reusable by the CSV export

--- lib/scrumboard.lib.php
+++ lib/scrumboard.lib.php
@@ -274,3 +274,119 @@ function getSQLForTasks(
 
 	return $sql;
 }
+
+	/**
+	 * @param DoliDB $db
+	 * @param int    $id_task
+	 * @param array  $values
+	 * @return array  Associative array with all the task details the scrumboard card needs
+	 */
+function getTaskDetailsForScrumboardCard(&$db, $id_task, $values=array()) {
+	global $user, $langs,$conf;
+
+	$task=new Task($db);
+	if($id_task) $task->fetch($id_task);
+
+	$sql = 'SELECT sourcetype, fk_source
+		FROM ' . MAIN_DB_PREFIX . 'element_element
+		WHERE targettype = "' . $task->element . '"
+		AND fk_target = ' . intval($task->id);
+
+	$resql = $db->query($sql);
+
+	$obj = $db->fetch_object($resql);
+
+	// Méthodes sur les commentaires ajoutées en standard depuis la 7.0
+	if(! empty($conf->global->PROJECT_ALLOW_COMMENT_ON_TASK) && empty($task->comments) && method_exists($task, 'fetchComments')) $task->fetchComments();
+
+	if(! empty($obj)) {
+		$sourcetype = $obj->sourcetype;
+		$fk_line = $obj->fk_source;
+
+		if($sourcetype == 'orderline') $line = new OrderLine($db);
+		else if($sourcetype == 'propaldet') $line = new PropaleLigne($db);
+
+		if(! empty($line) && ! empty($fk_line)) $line->fetch($fk_line);
+
+		if($sourcetype == 'orderline') {
+			$task->origin = 'order';
+			$task->origin_id = $line->fk_commande;
+		}
+		else if($sourcetype == 'propaldet') {
+			$task->origin = 'propal';
+			$task->origin_id = $line->fk_propal;
+		}
+	}
+
+	if(!empty($values)){
+		_set_values($task, $values);
+
+		if($values['status']=='inprogress') {
+			if($task->progress==0)$task->progress = 5;
+			else if($task->progress==100)$task->progress = 95;
+		}
+		else if($values['status']=='finish') {
+			$task->progress = 100;
+		}
+		else if($values['status']=='todo') {
+			$task->progress = 0;
+		}
+
+		$task->status = $values['status'];
+		$task->update($user);
+
+		$db->query("UPDATE ".MAIN_DB_PREFIX.$task->table_element."
+			SET story_k=".(int)$values['story_k']."
+			,scrum_status='".$values['scrum_status']."'
+		WHERE rowid=".$task->id);
+	}
+
+	// Méthodes sur les commentaires ajoutées en standard depuis la 7.0
+	if(!empty($conf->global->PROJECT_ALLOW_COMMENT_ON_TASK) && method_exists($task, 'getNbComments')) {
+		$task->nbcomment = $task->getNbComments();
+	}
+
+	$task->date_delivery = 0;
+	if($task->date_end >0 && $task->planned_workload>0) {
+
+		$velocity = scrum_getVelocity($db, $task->fk_project);
+		$task->date_delivery = _get_delivery_date_with_velocity($db, $task, $velocity);
+
+	}
+
+//    $timespentoutputformat='all';
+//    if (! empty($conf->global->PROJECT_TIMES_SPENT_FORMAT)) $timespentoutputformat=$conf->global->PROJECT_TIME_SPENT_FORMAT;
+	$working_timespentoutputformat='all';
+	if (! empty($conf->global->PROJECT_WORKING_TIMES_SPENT_FORMAT)) $working_timespentoutputformat=$conf->global->PROJECT_WORKING_TIMES_SPENT_FORMAT;
+
+	$working_days_per_weeks=7;
+	$dayInSecond = 86400;
+	if (!empty($conf->global->PROJECT_WORKING_HOURS_PER_DAY))
+	{
+		$working_days_per_weeks=!empty($conf->global->PROJECT_WORKING_DAYS_PER_WEEKS) ? $conf->global->PROJECT_WORKING_DAYS_PER_WEEKS : 5;
+		$working_hours_per_day=!empty($conf->global->PROJECT_WORKING_HOURS_PER_DAY) ? $conf->global->PROJECT_WORKING_HOURS_PER_DAY : 7;
+		$working_hours_per_day_in_seconds = 3600 * $working_hours_per_day;
+		$dayInSecond = $working_hours_per_day_in_seconds;
+	}
+	elseif($conf->global->SCRUM_DEFAULT_VELOCITY){
+		$dayInSecond = 60*60*$conf->global->SCRUM_DEFAULT_VELOCITY;
+	}
+
+	$task->aff_time = convertSecondToTime($task->duration_effective,$working_timespentoutputformat,$dayInSecond, $working_days_per_weeks);
+	$task->aff_planned_workload = convertSecondToTime($task->planned_workload,$working_timespentoutputformat,$dayInSecond, $working_days_per_weeks);
+
+	$task->long_description.='';
+	if(!empty($conf->global->SCRUM_SHOW_DATES_IN_DESCRIPTION)) {
+		if($task->date_start>0) $task->long_description .= $langs->trans('TaskDateStart').' : '.dol_print_date($task->date_start).'<br />';
+		if($task->date_end>0) $task->long_description .= $langs->trans('TaskDateEnd').' : '.dol_print_date($task->date_end).'<br />';
+		if($task->date_delivery>0 && $task->date_delivery>$task->date_end) $task->long_description .= $langs->trans('TaskDateShouldDelivery').' : '.dol_print_date($task->date_delivery).'<br />';
+	}
+	$task->long_description.=nl2br($task->description);
+
+	if (!empty($conf->global->SCRUM_SHOW_LINKED_CONTACT)) _getTContact($task);
+
+	$task->formatted_date_start_end = '';
+	if (!empty($conf->global->SCRUM_SHOW_DATES)) $task->formatted_date_start_end = dol_print_date($task->date_start, 'day') . ' - ' . dol_print_date($task->date_end, 'day');
+
+	return _as_array($task);
+}
