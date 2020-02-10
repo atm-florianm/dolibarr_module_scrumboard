7d2ed8d6ff3b52b683d3403f6dc98d1f49ca1353
Florian Mortgat <florian.mortgat@atm-consulting.fr>
2020-02-10 11:07:04 +0100

    T1981 - enable scrumboard link to origin documents when the origin document is a contract

--- script/scrum.js.php
+++ script/scrum.js.php
@@ -244,8 +244,9 @@ function project_refresh_task(id_project, task) {
 		pop_contact( $('#scrum').attr('id_projet'), task.id );
 	});
 
-	if(task.origin == 'order') $item.find('.task-origin a').attr('href', '<?php echo dol_buildpath('commande/card.php', 1); ?>?id='+task.origin_id);
-	else if(task.origin == 'propal') $item.find('.task-origin a').attr('href', '<?php echo dol_buildpath('comm/propal/card.php', 1); ?>?id='+task.origin_id);
+	if     (task.origin == 'order')   $item.find('.task-origin a').attr('href', '<?php echo dol_buildpath('commande/card.php', 1); ?>?id='+task.origin_id);
+	else if(task.origin == 'propal')  $item.find('.task-origin a').attr('href', '<?php echo dol_buildpath('comm/propal/card.php', 1); ?>?id='+task.origin_id);
+	else if(task.origin == 'contrat') $item.find('.task-origin a').attr('href', '<?php echo dol_buildpath('contrat/card.php', 1); ?>?id='+task.origin_id);
 	else $item.find('.task-origin a').remove();
 
 	<?php
